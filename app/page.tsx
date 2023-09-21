d4e61ee2e49c3e83086fd69ce6f344520780241b
shahin1290 <s.patowary@yahoo.com>
2023-09-21 11:02:12 +0200

    initial main page animation with framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Banner from "./components/Banner";
+import { Collaboration } from "./components/Collaboration";
 import HealthcareBanner from "./components/HealthcareBanner";
 import HealthcareList from "./components/HealthcareList";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <header className="px-16 mx-auto"><Banner /></header>      
+      {/* <Hero /> */}
+      <div className="relative z-10 w-full overflow-x-clip">
+        <Collaboration />
+      </div>
+      <header className="px-16 mx-auto">
+        <Banner />
+      </header>
       <HealthcareBanner />
       <HealthcareList />
     </main>
