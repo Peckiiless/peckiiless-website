e25c7a5453c1359452776d97c0d10c93859d5eb6
shahin1290 <s.patowary@yahoo.com>
2023-09-26 17:07:45 +0200

    adds locomotive scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { useEffect } from "react";
 import Banner from "./components/Banner";
 import { Collaboration } from "./components/Collaboration";
 import HealthcareBanner from "./components/HealthcareBanner";
 import HealthcareList from "./components/HealthcareList";
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
   return (
     <main>
       <div className="">
