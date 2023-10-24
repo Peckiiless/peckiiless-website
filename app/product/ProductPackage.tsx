f94cfca7720c07f43ae679ad652a76e339b311f3
shahin1290 <s.patowary@yahoo.com>
2023-10-25 00:11:27 +0200

    update product page

--- app/product/ProductPackage.tsx
+++ app/product/ProductPackage.tsx
@@ -2,26 +2,21 @@ import Image from "next/image";
 
 export default function ProductPackage() {
   return (
-    <div className="p-20">
-      <div className="grid md:grid-cols-2 place-items-center gap-20">
-        <div className="">
+    <div className="package-graidient">
+      <div className="grid md:grid-cols-5  gap-2">
+        <div className="col-span-3">
           <Image
             src="/package.png"
-            width={768}
-            height={464}
+            width={1440}
+            height={599}
             alt="logo"
             priority={true}
           />
         </div>
-        <div>
-          <p className="font-bold text-[3.75rem] text-primary-800  ">
-            Peckiiless Breathalyzer
-          </p>
-          <p className="text-[1.625rem]   font-medium text-right pl-36">
-            The breathalyzer connects seeminglessly though our mobile appication
-            where you can easily monitor 8 different pathogens in your breath.
-          </p>
-        </div>
+        <p className="text-[1.625rem]  place-self-center font-medium text-right pr-10 col-span-2">
+          The breathalyzer connects seeminglessly though our mobile appication
+          where you can easily monitor 8 different pathogens in your breath.
+        </p>
       </div>
     </div>
   );
