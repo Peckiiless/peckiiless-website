8f3612535e3bf3034da3200682ab3eeb42543033
shahin1290 <s.patowary@yahoo.com>
2023-11-12 22:07:01 +0100

    update healthcare design

--- app/healthcare/Pregnancy.tsx
+++ app/healthcare/Pregnancy.tsx
@@ -2,31 +2,39 @@ import Image from "next/image";
 
 export default function Pregnancy() {
   return (
-    <section className="bg-patternLady bg-no-repeat bg-contain  bg-bottom">
-      <p className="font-medium text-[3.125rem] text-primary-800 leading-[3.75rem] p-20 w-[100%]">
-        Here at Pickiless we have created the one of the first advanced
-        breathalyzer that can monitor your glucose levels.
+    <section className="">
+      <p className="text-center font-medium text-[2.625rem] text-primary-800 leading-[3.25rem] py-36 w-[81rem] mx-auto">
+        Both the number of cases and the prevalence of diabetes have been
+        steadily increasing over the past few decades. Testing and monitoring of
+        these illnesses have gotten stuck in development for years upon years,
+        it’s time to globalize an easier option for monitoring your glucose
+        testing in an easy, effecient way.
       </p>
-      <div className="grid sm:grid-cols-2 ">
-        <div className="px-20 justify-self-end">
+      <div className="grid sm:grid-cols-12 ">
+        <div className="col-span-5 justify-self-center">
           <Image
             src="/pregnant.png"
-            width={450}
-            height={600}
+            width={388}
+            height={822}
             alt="logo"
             priority={true}
           />
         </div>
-        <div className="text-right px-12 py-10">
+        <div className="text-right col-span-7 px-[14%] self-center">
           <p className="font-medium text-[3.125rem] text-primary-800 leading-[3.75rem] pb-10">
             Living with Diabetes
           </p>
-          <p className="text-[1.625rem] ">
-            Pregnant women living with diabetes increases risks of
-            complications, having a cesarean delivery, increases babies’ risk of
-            being born too large and developing obesity or type 2 diabetes in
-            the future. But by using breathalyzer she has a less intrusive way
-            of monitor her glucose levels multiple times a day.
+          <p className="text-[1.75rem] pb-10">
+            Moreover, women diagnosed with gestational diabetes often need to
+            prick their fingers up to 7 times a day, causing painful disruptions
+            to their daily life and contributing to the disposal of billions of
+            one-time-use needles and test strips every day.
+          </p>
+          <p className="text-[1.75rem] ">
+            In addition, people with chronic diseases often need to monitor
+            vital factors regularly. Blood-based health checks can be
+            inaccessible and difficult to track, making it challenging to
+            optimize personal treatment and prevent complications.
           </p>
         </div>
       </div>
--- app/healthcare/ProductBanner.tsx
+++ app/healthcare/ProductBanner.tsx
@@ -15,9 +15,22 @@ export default function ProductBanner() {
     //   </div>
     // </section>
 
-    <div className="">
-      <div className="grid md:grid-cols-2 gap-2">
-        <div className="place-self-center pl-20 pt-20">
+    <div className="pt-20">
+      <p className="text-center font-medium text-[2.625rem] text-primary-800 leading-[3.25rem] py-36 w-[81rem] mx-auto">
+        At Peckiiless, we unlock the potential of Exhalation Breath, delivering
+        non-invasive comfort to everyone worldwide
+      </p>
+      <div className="grid sm:grid-cols-12">
+        <div className="col-span-7">
+          <Image
+            src="/product.png"
+            width={1000}
+            height={464}
+            alt="logo"
+            priority={true}
+          />
+        </div>
+        <div className="col-span-5 px-[14%] self-center">
           <p className="font-bold text-[3.125rem] text-primary-800 pb-10">
             Breathalyzer
           </p>
@@ -27,22 +40,12 @@ export default function ProductBanner() {
             mobile application.
           </p>
 
-          <div className="place-self-center lg:col-span-4 p-20">
+          <div className="lg:col-span-4 py-20">
             <CustomButton
               title="More Information"
               containerStyles="bg-primary-700 font-medium text-[1.625rem] text-white rounded-[2.5rem] py-7 px-12 leading-10"
             />
           </div>
-        </div>
-
-        <div className="">
-          <Image
-            src="/product.png"
-            width={1000}
-            height={464}
-            alt="logo"
-            priority={true}
-          />
         </div>
       </div>
     </div>
