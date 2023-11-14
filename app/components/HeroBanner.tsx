bb5ab010ef6ca986404edd9fa6de0b703d2adb4f
shahin1290 <s.patowary@yahoo.com>
2023-11-14 22:08:37 +0100

    update homepage banner

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -2,18 +2,25 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <section className="graidient relative text-white h-screen max-h-[46.625rem]">     
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <section className="relative text-white h-[30vw] lg:h-[50vw]">
+      <Image
+        src="/curve.svg"
+        alt="Image Alt Text"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full absolute -top-[4.6875rem] -z-10"
+      />
+      <div className="absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Image
           src="pecki-logo-icon.svg"
           width={240}
           height={368}
           alt="logo"
           priority={true}
-          
         />
       </div>
-       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Image
           src="pecki-logo-text.svg"
           width={933}
@@ -22,7 +29,7 @@ export default function Banner() {
           priority={true}
         />
       </div>
-      <p className="absolute top-[67%] left-[69%] -translate-x-1/2 -translate-y-1/2 w-[17rem] text-[1.75rem] font-medium">
+      <p className="absolute top-[60%] left-[69%] -translate-x-1/2 -translate-y-1/2 w-[17rem] text-[1.75rem] font-medium">
         A solution for people living with diabetes.
       </p>
     </section>
--- app/components/PreLoader.tsx
+++ app/components/PreLoader.tsx
@@ -13,7 +13,7 @@ const PreLoader = () => {
     })
       .from(".texts-container .text-animation", {
         duration: 1.5,
-        delay: 5,
+        delay: 3,
         stagger: 0.4,
         ease: "Power3.easeOut",
       })
