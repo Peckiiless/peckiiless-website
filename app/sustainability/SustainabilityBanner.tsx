98cee46be80630894bcda373a55b0d793fd9ea2a
shahin1290 <s.patowary@yahoo.com>
2023-11-21 22:47:18 +0100

    update gear animation

--- app/sustainability/SustainabilityBanner.tsx
+++ app/sustainability/SustainabilityBanner.tsx
@@ -2,26 +2,26 @@ import Image from "next/image";
 
 export default function SustainabilityBanner() {
   return (
-    <div className="px-20 my-20">
-      <div className="flex">
+    <div className="px-20 my-10 h-[50rem] max-w-[65rem] relative mx-auto">
+      <div className="">
         <div className="">
           <Image
             src="/industry-banner.png"
-            width={850}
-            height={850}
+            width={600}
+            height={600}
             alt="medicin with trolley"
             priority={true}
-            className=" spin"
+            className="spin absolute left-0 top-0"
           />
         </div>
         <div className="">
           <Image
             src="/industry-banner2.png"
-            width={850}
-            height={850}
+            width={400}
+            height={400}
             alt="medicin with trolley"
             priority={true}
-            className="spin-back"
+            className="spin-back absolute right-0 top-[30%]"
           />
         </div>
       </div>
