3772bb3e60706387bcb4b02cdddce1ba03878812
shahin1290 <s.patowary@yahoo.com>
2023-11-30 21:10:08 +0100

    update etty

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export const MainPage = () => {
   return (
-    <div className="relative h-[94rem] max-w-[90rem] overflow-hidden flex justify-center mx-auto pr-32">
-      <div className="absolute -top-32">
+    <div className="relative max-w-[90rem] overflow-hidden flex justify-center mx-auto pr-32">
+      <div className="scale-105">
         <Image
           src="/etty-fidele.png"
-          width={1200}
-          height={1200}
+          width={1500}
+          height={1500}
           alt="logo"
           priority={true}
           quality={100}
