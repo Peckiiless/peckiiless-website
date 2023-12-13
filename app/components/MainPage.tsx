863e80d7afa25ca9dc0058899a9208eaeeebfe96
shahin1290 <s.patowary@yahoo.com>
2023-12-13 20:36:37 +0100

    use hq images

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export const MainPage = () => {
   return (
-    <div className="relative max-w-[90rem] overflow-hidden flex justify-center mx-auto pr-32">
-      <div className="scale-105">
+    <div className="relative max-w-[90rem] overflow-hidden flex justify-center mx-auto">
+      <div className="scale-[1.02]">
         <Image
           src="/etty-fidele.svg"
-          width={1500}
-          height={1500}
+          width={1100}
+          height={1100}
           alt="logo"
           priority={true}
           quality={100}
