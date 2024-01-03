41df0daf35727314d6ab66b21c9cd154582ced05
shahin1290 <s.patowary@yahoo.com>
2024-01-03 09:42:37 +0100

    update

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -20,7 +20,7 @@ export default function AboutUsPage() {
       <Partners />
       <Awards />
       <Supports />
-      <SubmitForm />
+      {/* <SubmitForm /> */}
       <Footer />
     </div>
   );
