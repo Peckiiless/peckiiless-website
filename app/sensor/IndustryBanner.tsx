8a5ac6611a1370727d01740e9768accc1c4bca03
shahin1290 <s.patowary@yahoo.com>
2024-01-05 12:40:17 +0100

    update industry page

--- app/sensor/IndustryBanner.tsx
+++ app/sensor/IndustryBanner.tsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 
 export default function IndustryfoBanner() {
   return (
-    <div className="px-20 pt-10">
-      <div className="md:flex justify-between items-center gap-8">
-        <div className="text-red-900 font-bold text-5xl">Industry Text</div>
-        <div className="">
-          <Image
-            src="/industry-banner.png"
-            width={450}
-            height={400}
-            alt="medicin with trolley"
-            priority={true}
-          />
-        </div>
-      </div>
+    <div className="h-[39vw] md:h-[45vw] lg:h-[50vw]">      
+
+      <Image
+        src="/workers.svg"
+        alt="Image Alt Text"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full absolute top-0 -z-10"
+        priority={true}
+      />
     </div>
   );
 }
