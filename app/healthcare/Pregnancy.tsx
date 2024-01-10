d9f2c6cb3c8274ca239e1a7895113fe09811a055
shahin1290 <s.patowary@yahoo.com>
2024-01-10 21:41:24 +0100

    update pregnancy component

--- app/healthcare/Pregnancy.tsx
+++ app/healthcare/Pregnancy.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Pregnancy() {
   return (
-    <section>
+    <section className="mx-auto max-w-[100rem]">
       <div className="px-[2%]">
         <p className="text-center font-medium xs:text-[1.625rem] md:text-[1.75rem] text-[1rem]  leading-[1.5rem] xs:leading-[2.5rem]  md:leading-[2.625rem] my-20 px-1 py-14 xs:px-10 max-w-[85rem] mx-auto">
           In the world of health monitoring, current tools have long leaned on
@@ -16,25 +16,25 @@ export default function Pregnancy() {
         </p>
       </div>
       <div className="grid grid-cols-12 ">
-        <div className=" text-right col-span-7 pt-16 pb-24  md:px-[4rem] px-1 self-center xs:text-[1.25rem] md:text-[1.75rem] text-[1rem]">
-          <p className="font-medium xs:text-[2rem] md:text-[2.875rem] text-[1.375rem] text-primary-800 leading-[3.75rem] pb-2 lg:pb-10">
+        <div className=" col-span-8 md:col-span-7 lg:col-span-7 xl:col-span-8 2xl:col-span-7 text-right pt-16 pb-24  md:px-[3rem] px-1 self-center xs:text-[1.15rem] sm:text-[1.35rem] md:text-[1.20rem] lg:text-[1.45rem] xl:text-[1.75rem] 2xl:text-[1.75rem] text-[1rem]">
+          <p className="font-medium xs:text-[2rem] md:text-[2.875rem] text-[1.375rem] text-primary-800 leading-[3.75rem]  xs:pb-8 xl:pb-10">
             Living with Diabetes
           </p>
-          <p className="pb-10">
+          <p className="pb-1 xs:pb-5 md:pb-10">
             For women diagnosed with gestational (pregnancy) diabetes, the daily
             ritual of pricking their fingers up to 7 times is more than a health
             check-it&#39;s a disruptive, painful pause in their routine,
             contributing to the staggering disposal of billions of one-time-use
             needles and test strips every day.
           </p>
-          <p className="hidden xs:block pb-10">
+          <p className="hidden sm:block pb-1 xs:pb-5 md:pb-10">
             In the world of chronic diseases, the need for regular monitoring is
             undeniable. However, traditional blood-based health checks pose
             accessibility challenges and tracking difficulties, hindering the
             optimization of personal treatment and the prevention of potential
             complications.
           </p>
-          <p className="hidden xs:block">
+          <p className="hidden md:block">
             Discovering a new era in health monitoring that goes beyond
             inconvenience is crucial. Saying goodbye to constant pricking and
             hello to a seamless, accessible approach empowers everyone to take
@@ -43,12 +43,18 @@ export default function Pregnancy() {
             monitoring becomes a part of everybody&#39;s lifestyle, not a
             disruption.
           </p>
+          <p className=" md:hidden font-medium text-[1rem] cursor-pointer py-1">
+             Read more
+          </p>
         </div>
-        <div className="col-span-5 justify-self-center pr-3 mt-20">
+        <div
+          style={{ width: "85%", height: "85%", position: "relative" }}
+          className="col-span-4 md:col-span-5 lg:col-span-5 xl:col-span-4 2xl:col-span-5 justify-self-center pr-3 mt-14 xs:mt-20"
+        >
           <Image
             src="/pregnant.png"
-            width={450}
-            height={450}
+            layout="fill"
+            objectFit="contain"
             alt="logo"
             priority={true}
             quality={100}
