ecd5d9bf3bba0743843c54a168ddaa27837143c0
shahin1290 <s.patowary@yahoo.com>
2024-01-18 13:58:12 +0100

    fix font size

--- app/sustainability/Benefits.tsx
+++ app/sustainability/Benefits.tsx
@@ -7,11 +7,11 @@ const Benefits = ({ view }: { view: string }) => {
   return (
     <div id="first-section" className="p-[2%]">
       <div className="">
-        <p className="text-center font-medium xs:text-[2rem] text-[1.375rem] text-primary-800 leading-[1.875rem] xs:leading-[2.625rem] py-6 max-w-[75rem] mx-auto px-2">
+        <p className="text-center font-medium sm:text-[1.625rem] text-[1.25rem] md:text-[2rem] text-primary-800 leading-[1.875rem] xs:leading-[2.625rem] py-6 max-w-[75rem] mx-auto px-2">
           At Peckiiless, sustainability is the powerful gear that propels our
           technology towards a brighter future.
         </p>
-        <p className="text-center font-medium xs:text-[1.875rem] text-[1.375rem] leading-[1.875rem] xs:leading-[2.625rem] py-6 max-w-[80rem] mx-auto px-2">
+        <p className="text-center font-medium xs:text-[1.875rem] md:text-[1.25rem] leading-[1.875rem] xs:leading-[2.625rem] py-6 max-w-[80rem] mx-auto px-2">
           Sustainability is the beating heart of our innovation and technology.
           We hold a deep love for our environment, a genuine concern for health
           and happiness, a profound commitment to sustainable industry, and an
@@ -27,7 +27,7 @@ const Benefits = ({ view }: { view: string }) => {
           )}
         </div>
         <div className="md:hidden">
-            {view === "breathalyzer" ? (
+          {view === "breathalyzer" ? (
             <BenefitsMobile />
           ) : (
             <BenefitsSensorMobile />
