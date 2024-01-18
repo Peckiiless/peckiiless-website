6fb1eadfd905da9146eb2914438b30e3fc11caa3
shahin1290 <s.patowary@yahoo.com>
2024-01-18 13:36:23 +0100

    footer update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,6 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Logo from "./Logo";
 import SubmitForm from "../about-us/components/SubmitForm";
-import FadeIn, { FadeInStagger } from "@/app/components/FadeIn";
 
 const Footer = () => {
   const currentRoute = usePathname();
@@ -21,14 +20,13 @@ const Footer = () => {
         <div className={`${currentRoute !== "/about-us" && "hidden"}`}>
           <SubmitForm />
         </div>
-        <FadeInStagger
-          faster
+        <div
           className={`${
             currentRoute === "/about-us" && "hidden"
           } flex justify-center px-4`}
         >
           <div className="flex gap-6  mt-6 xs:mt-14">
-            <FadeIn className="flex gap-2 items-center">
+            <div className="flex gap-2 items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="25"
@@ -44,31 +42,55 @@ const Footer = () => {
               </svg>
 
               <p className="">info@peckiiless.com</p>
-            </FadeIn>
+            </div>
 
-            <FadeIn className="flex gap-2 items-center">
-             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-  <rect width="25" height="25" rx="6" fill="#B2335E"/>
-  <path d="M11.4121 10.1641H13.8568V11.4169H13.8921C14.2322 10.8064 15.0645 10.1641 16.3058 10.1641C18.8865 10.1641 19.3636 11.7703 19.3636 13.8597V18.1156H16.8147V14.343C16.8147 13.4436 16.7962 12.2853 15.4895 12.2853C14.1624 12.2853 13.9592 13.2651 13.9592 14.2776V18.1156H11.4121V10.1641Z" fill="white"/>
-  <path d="M6.99414 10.1641H9.64464V18.1156H6.99414V10.1641Z" fill="white"/>
-  <path d="M9.64464 7.9522C9.64464 8.68374 9.05093 9.27745 8.31939 9.27745C7.58785 9.27745 6.99414 8.68374 6.99414 7.9522C6.99414 7.22066 7.58785 6.62695 8.31939 6.62695C9.05093 6.62695 9.64464 7.22066 9.64464 7.9522Z" fill="white"/>
-</svg>
+            <div className="flex gap-2 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+              >
+                <rect width="25" height="25" rx="6" fill="#B2335E" />
+                <path
+                  d="M11.4121 10.1641H13.8568V11.4169H13.8921C14.2322 10.8064 15.0645 10.1641 16.3058 10.1641C18.8865 10.1641 19.3636 11.7703 19.3636 13.8597V18.1156H16.8147V14.343C16.8147 13.4436 16.7962 12.2853 15.4895 12.2853C14.1624 12.2853 13.9592 13.2651 13.9592 14.2776V18.1156H11.4121V10.1641Z"
+                  fill="white"
+                />
+                <path
+                  d="M6.99414 10.1641H9.64464V18.1156H6.99414V10.1641Z"
+                  fill="white"
+                />
+                <path
+                  d="M9.64464 7.9522C9.64464 8.68374 9.05093 9.27745 8.31939 9.27745C7.58785 9.27745 6.99414 8.68374 6.99414 7.9522C6.99414 7.22066 7.58785 6.62695 8.31939 6.62695C9.05093 6.62695 9.64464 7.22066 9.64464 7.9522Z"
+                  fill="white"
+                />
+              </svg>
 
               <p className="">Peckiiless</p>
-            </FadeIn>
-            <FadeIn className="flex gap-2 items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-  <rect width="25" height="25" rx="6" fill="#B2335E"/>
-  <path d="M12.4984 4.60156C9.68222 4.60156 7.39844 6.7975 7.39844 9.50541C7.39844 14.4093 12.4984 20.2939 12.4984 20.2939C12.4984 20.2939 17.5984 14.4093 17.5984 9.50541C17.5984 6.7975 15.3147 4.60156 12.4984 4.60156ZM12.4984 12.4477C10.8083 12.4477 9.43844 11.1305 9.43844 9.50541C9.43844 7.88027 10.8083 6.5631 12.4984 6.5631C14.1886 6.5631 15.5584 7.88027 15.5584 9.50541C15.5584 11.1305 14.1886 12.4477 12.4984 12.4477Z" fill="white"/>
-</svg>
+            </div>
+            <div className="flex gap-2 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+              >
+                <rect width="25" height="25" rx="6" fill="#B2335E" />
+                <path
+                  d="M12.4984 4.60156C9.68222 4.60156 7.39844 6.7975 7.39844 9.50541C7.39844 14.4093 12.4984 20.2939 12.4984 20.2939C12.4984 20.2939 17.5984 14.4093 17.5984 9.50541C17.5984 6.7975 15.3147 4.60156 12.4984 4.60156ZM12.4984 12.4477C10.8083 12.4477 9.43844 11.1305 9.43844 9.50541C9.43844 7.88027 10.8083 6.5631 12.4984 6.5631C14.1886 6.5631 15.5584 7.88027 15.5584 9.50541C15.5584 11.1305 14.1886 12.4477 12.4984 12.4477Z"
+                  fill="white"
+                />
+              </svg>
               <p className="">Chalmersplatsen 4, 412 96 Göteborg</p>
-            </FadeIn>
+            </div>
           </div>
-        </FadeInStagger>
+        </div>
       </div>
-      <FadeIn className="text-center py-5 xs:py-10">
+      <div className="text-center py-5 xs:py-10">
         @2023 www.peckiiless.com all copyrights reserved.
-      </FadeIn>
+      </div>
       <div className="bg-footer h-[5rem] sm:h-[15rem] bg-bottom xs:bg-top bg-contain xs:bg-cover bg-no-repeat"></div>
     </div>
   );
