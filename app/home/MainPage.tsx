ede4368c7c5cc10ad5d0fdb3eb5b42f7a9dfeb45
shahin1290 <s.patowary@yahoo.com>
2024-01-22 15:41:03 +0100

    etty image blur

--- app/home/MainPage.tsx
+++ app/home/MainPage.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
+import etty from "../../public/main-screen.png"
+
+
 
 export const MainPage = () => {
   return (
-    <div className="relative max-w-[90rem] overflow-hidden flex justify-center mx-auto">
+    <div className="relative min-h-screen max-w-[70rem] overflow-hidden flex justify-center mx-auto">
       <div>
         <Image
-          src="/etty-fidele.svg"
-          width={1100}
-          height={1100}
+          src={etty}
+          width={1400}
+          height={2413}
           alt="logo"
           priority={true}
+          quality={100}
+          placeholder="blur"
         />
       </div>
     </div>
