d9feb80dc3a2354d0c6c2e7770c26a2907c7b092
shahin1290 <s.patowary@yahoo.com>
2024-01-31 09:29:48 +0100

    fix

--- app/home/MainPage.tsx
+++ app/home/MainPage.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import etty from "../../public/main-screen.png";
 import { Text2rem } from "../components/Text";
+import FadeIn from "../components/FadeIn";
 
 export const MainPage = () => {
   return (
@@ -18,34 +19,33 @@ export const MainPage = () => {
             className="px-6"
           />
         </div>
-        <div className="text-nav">
-        <div className="absolute top-[20%] left-[56%]">
-          <Text2rem>
-            <div>From a Silent Breath to</div>
-            <div className="font-semibold">Peace of Mind</div>
-          </Text2rem>
-        </div>
-        <div className="absolute top-[32%] left-[0%]">
-          <Text2rem>
-            <div>From Peace of Mind to</div>
-            <div className="font-semibold">Vibrant Health</div>
-          </Text2rem>
-        </div>
-        <div className="absolute bottom-[38%] right-[0%]">
-          <Text2rem>
-            <div>From Vibrant Health to</div>
-            <div className="font-semibold">Industrial Harmony</div>
-          </Text2rem>
-        </div>
-        <div className="absolute bottom-[25%] left-[5%] ">
-          <Text2rem>
-            <div>From Each to</div>
-            <div className="font-semibold"  >Global Impact</div>
-          </Text2rem>
+        <div className="text-nav hidden md:block">
+          <FadeIn className="absolute top-[20%] left-[56%]">
+            <Text2rem>
+              <div>From a Silent Breath to</div>
+              <div className="font-semibold">Peace of Mind</div>
+            </Text2rem>
+          </FadeIn>
+          <FadeIn className="absolute top-[32%] left-[0%]">
+            <Text2rem>
+              <div>From Peace of Mind to</div>
+              <div className="font-semibold text-right">Vibrant Health</div>
+            </Text2rem>
+          </FadeIn>
+          <FadeIn className="absolute bottom-[38%] right-[0%]">
+            <Text2rem>
+              <div>From Vibrant Health to</div>
+              <div className="font-semibold">Industrial Harmony</div>
+            </Text2rem>
+          </FadeIn>
+          <FadeIn className="absolute bottom-[25%] left-[5%] ">
+            <Text2rem>
+              <div>From Each to</div>
+              <div className="font-semibold">Global Impact</div>
+            </Text2rem>
+          </FadeIn>
         </div>
       </div>
-      </div>
-      
     </div>
   );
 };
