459b49cf06721d88d4119bf0b5dedf340b9a27b2
shahin1290 <s.patowary@yahoo.com>
2024-02-01 18:49:21 +0100

    update spash design

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -6,119 +6,119 @@ type Props = {
 
 export const Title1 = ({ title }: { title: string }) => {
   return (
-    <p className="text-center font-medium leading-[1.875rem] sm:leading-[2.75rem] lg:leading-[3.875rem] sm:text-[2rem] lg:text-[2.875rem] text-[1.25rem] text-primary-700">
+    <div className="text-center font-medium leading-[1.875rem] sm:leading-[2.75rem] lg:leading-[3.875rem] sm:text-[2rem] lg:text-[2.875rem] text-[1.25rem] text-primary-700">
       {title}
-    </p>
+    </div>
   );
 };
 
 export const Title2 = ({ children }: Props) => {
   return (
-    <p className="font-medium sm:text-[1.625rem] xl:text-[1.75rem] text-[1rem] leading-[1.5rem] sm:leading-[2.5rem] lg:leading-[2.625rem]">
+    <div className="font-medium sm:text-[1.625rem] xl:text-[1.75rem] text-[1rem] leading-[1.5rem] sm:leading-[2.5rem] lg:leading-[2.625rem]">
       {children}
-    </p>
+    </div>
   );
 };
 export const Title3625 = ({ children }: Props) => {
   return (
-    <p className=" xl:text-[3.625rem] xl:text-[3rem] text-[1.25rem] leading-[1.125rem] sm:text-[2.625rem] sm:leading-[3rem] font-bold text-primary-800">
+    <div className=" xl:text-[3.625rem] xl:leading-[3rem] text-[1.25rem] leading-[1.125rem] sm:text-[2.625rem] sm:leading-[3rem] font-bold text-primary-800">
       {children}
-    </p>
+    </div>
   );
 };
 
 export const Title2rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[2rem] xl:leading-[2.75rem] md:text-[1.5rem] md:leading-[2rem] xs:text-[1rem] xs:leading-[1.5rem] text-[.835rem] leading-[1rem] font-medium text-primary-700">
+    <div className="xl:text-[2rem] xl:leading-[2.75rem] md:text-[1.5rem] md:leading-[2rem] xs:text-[1rem] xs:leading-[1.5rem] text-[.835rem] leading-[1rem] font-medium text-primary-700">
       {children}
-    </p>
+    </div>
   );
 };
 export const Title2625rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[2.625rem] xl:leading-[3.5rem]  md:text-[2rem] md:leading-[2.75rem]  text-[1.25rem] leading-[1.5rem] text-primary-700 text-center font-medium">
+    <div className="xl:text-[2.625rem] xl:leading-[3.5rem]  md:text-[2rem] md:leading-[2.75rem]  text-[1.25rem] leading-[1.5rem] text-primary-700 text-center font-medium">
       {children}
-    </p>
+    </div>
   );
 };
 export const Title2875rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[2.875rem] xl:leading-[3.375rem] md:text-[2rem] md:leading-[2.5rem] text-[1.25rem] leading-[1.5rem] font-medium text-primary-700">
+    <div className="xl:text-[2.875rem] xl:leading-[3.375rem] md:text-[2rem] md:leading-[2.5rem] text-[1.25rem] leading-[1.5rem] font-medium text-primary-700">
       {children}
-    </p>
+    </div>
   );
 };
 
 export const Text1 = ({ children }: Props) => {
   return (
-    <p className="text-[.9375rem] leading-[1.3125rem] md:text-[1.25rem] sm:leading-[1.75rem] lg:text-[1.75rem] lg:leading-[2.5rem]">
+    <div className="text-[.9375rem] leading-[1.3125rem] md:text-[1.25rem] sm:leading-[1.75rem] lg:text-[1.75rem] lg:leading-[2.5rem]">
       {children}
-    </p>
+    </div>
   );
 };
 
 export const Text2 = ({ children }: Props) => {
   return (
-    <p className="md:text-[1.625rem] xl:text-[1.75rem] text-[1rem] leading-[1.5rem] md:leading-[2.5rem] xl:leading-[2.625rem]">
+    <div className="md:text-[1.625rem] xl:text-[1.75rem] text-[1rem] leading-[1.5rem] md:leading-[2.5rem] xl:leading-[2.625rem]">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text3 = ({ children }: Props) => {
   return (
-    <p className="md:text-[1.25rem] xl:text-[1.75rem] text-[.9375rem] leading-[1.375rem] md:leading-[1.875rem]  xl:leading-[2.6875rem] ">
+    <div className="md:text-[1.25rem] xl:text-[1.75rem] text-[.9375rem] leading-[1.375rem] md:leading-[1.875rem]  xl:leading-[2.6875rem] ">
       {children}
-    </p>
+    </div>
   );
 };
 
 export const Text2rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[2rem] xl:leading-[2.625rem] sm:text-[1.625rem] sm:leading-[2.5rem] text-[1.25rem] leading-[1.625rem]    ">
+    <div className="xl:text-[2rem] xl:leading-[2.625rem] sm:text-[1.625rem] sm:leading-[2.5rem] text-[1.25rem] leading-[1.625rem]    ">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text1875rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.875rem] xl:leading-[2.625rem] sm:text-[1.25rem] sm:leading-[2rem] text-[1rem] leading-[1.375rem]    ">
+    <div className="xl:text-[1.875rem] xl:leading-[2.625rem] sm:text-[1.25rem] sm:leading-[2rem] text-[1rem] leading-[1.375rem]    ">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text175rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.75rem] xl:leading-[2.5rem] md:text-[1.25rem] md:leading-[1.75rem] text-[.9375rem] leading-[1.25rem]">
+    <div className="xl:text-[1.75rem] xl:leading-[2.5rem] md:text-[1.25rem] md:leading-[1.75rem] text-[.9375rem] leading-[1.25rem]">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text1625rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.625rem] xl:leading-[2.5rem] md:text-[1.25rem] md:leading-[1.75rem] text-[.75rem] leading-[.875rem] font-medium">
+    <div className="xl:text-[1.625rem] xl:leading-[2.5rem] md:text-[1.25rem] md:leading-[1.75rem] text-[.75rem] leading-[.875rem] font-medium">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text125rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.25rem] xl:leading-[1.875rem] md:text-[1.125rem] md:leading-[1.375rem] text-[.75rem] leading-[.875rem]">
+    <div className="xl:text-[1.25rem] xl:leading-[1.875rem] md:text-[1.125rem] md:leading-[1.375rem] text-[.75rem] leading-[.875rem]">
       {children}
-    </p>
+    </div>
   );
 };
 export const Text1125rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.125rem] xl:leading-[1.25rem] md:text-[1rem] md:leading-[1.25rem] text-[.75rem] leading-[.875rem] font-light">
+    <div className="xl:text-[1.125rem] xl:leading-[1.25rem] md:text-[1rem] md:leading-[1.25rem] text-[.75rem] leading-[.875rem] font-light">
       {children}
-    </p>
+    </div>
   );
 };
 
 export const Text15rem = ({ children }: Props) => {
   return (
-    <p className="xl:text-[1.5rem] xl:leading-[2.5rem] md:text-[.875rem] md:leading-[1.75rem] text-[.75rem] leading-[.8125rem]">
+    <div className="xl:text-[1.5rem] xl:leading-[2.5rem] md:text-[.875rem] md:leading-[1.75rem] text-[.75rem] leading-[.8125rem]">
       {children}
-    </p>
+    </div>
   );
 };
--- app/home/MainPage.tsx
+++ app/home/MainPage.tsx
@@ -5,42 +5,47 @@ import FadeIn from "../components/FadeIn";
 
 export const MainPage = () => {
   return (
-    <div className=" px-4">
-      <div className="relative min-h-screen max-w-[94rem] overflow-hidden flex justify-center mx-auto">
-        <div>
-          <Image
-            src={etty}
-            width={900}
-            height={1600}
-            alt="logo"
-            priority={true}
-            quality={100}
-            className="lg:px-[10rem] xl:px-[10rem] 2xl:px-[2%]"
-          />
-        </div>
-        <div className="text-nav hidden lg:block">
-          <FadeIn className="absolute top-[20%] left-[56%]">
-            <Text1875rem>
-              <div>From a Silent Breath to</div>
-              <div className="font-semibold">Peace of Mind</div>
-            </Text1875rem>
-          </FadeIn>
-          <FadeIn className="absolute top-[32rem] left-[6%]">
+    <div className="px-4 max-w-[90rem] mx-auto">
+      <div className="grid grid-cols-12">
+        <div className="text-nav col-span-2 relative">
+          <FadeIn className="absolute top-[30%] -right-[60%] text-nowrap">
             <Text1875rem>
               <div>From Peace of Mind to</div>
               <div className="font-semibold text-right">Vibrant Health</div>
             </Text1875rem>
           </FadeIn>
-          <FadeIn className="absolute bottom-[38%] lg:left-[78%] xl:left-[73%] 2xl:left-[78%]">
-            <Text1875rem>
-              <div>From Vibrant Health to</div>
-              <div className="font-semibold">Industrial Harmony</div>
-            </Text1875rem>
-          </FadeIn>
-          <FadeIn className="absolute bottom-[25%] left-[5%] ">
+
+          <FadeIn className="absolute bottom-[15%] -right-[25%]">
             <Text1875rem>
               <div>From Each to</div>
               <div className="font-semibold">Global Impact</div>
+            </Text1875rem>
+          </FadeIn>
+        </div>
+        <div className="col-span-8 relative">
+          <Image
+            src={etty}
+            alt="logo"
+            priority={true}
+            quality={100}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="px-20"
+          />
+        </div>
+        <div className="text-nav col-span-2 relative">
+          <FadeIn className="absolute top-[10%] -left-[90%]">
+            <Text1875rem>
+              <div>From a Silent Breath to</div>
+              <div className="font-semibold">Peace of Mind</div>
+            </Text1875rem>
+          </FadeIn>
+
+          <FadeIn className="absolute bottom-[32%] -left-[30%]">
+            <Text1875rem>
+              <div>From Vibrant Health to</div>
+              <div className="font-semibold">Industrial Harmony</div>
             </Text1875rem>
           </FadeIn>
         </div>
