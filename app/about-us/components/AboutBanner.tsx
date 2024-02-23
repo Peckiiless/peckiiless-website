9a394965868230be51178493a56df48bed8055ba
shahin1290 <s.patowary@yahoo.com>
2024-02-23 17:15:17 +0100

    update about team png

--- app/about-us/components/AboutBanner.tsx
+++ app/about-us/components/AboutBanner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
   return (
     <section className="h-[39vw] md:h-[45vw] lg:h-[50vw]">
       <Image
-        src="/team.svg"
+        src="/team.png"
         alt="Image Alt Text"
         width={0}
         height={0}
